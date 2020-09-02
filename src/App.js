b1ae92ffc4d96fbc6d13bd02f74e21152ac50ce8
Maria Olanipekun <m.olanipekun@hotmail.com>
2020-09-02 21:51:07 +0100

    update app routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from "react";
-import Timer from "./pages/Timer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { PageContainer } from "./styled/Lib";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+// Pages
 import Homepage from "./pages/Homepage";
+import Timer from "./pages/Timer";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-      <Timer />
-    </div>
+    <Router>
+      <Route
+        render={({ location }) => {
+          return (
+            <PageContainer>
+              <TransitionGroup component={null}>
+                <CSSTransition
+                  timeout={300}
+                  classNames="page"
+                  key={location.key}
+                >
+                  <Switch location={location}>
+                    <Route exact path="/details" component={Timer} />
+                    <Route exact path="/" component={Homepage} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            </PageContainer>
+          );
+        }}
+      />
+    </Router>
   );
 }
 
